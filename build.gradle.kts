36593eadb2de81a2c75e54e4fb102b5c05614747
Lukas Sztefek <lukas.sztefek@gmail.com>
2020-05-14 17:10:25 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.4-M1"
+}
+
+group = "cz.skywall.kuple"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
